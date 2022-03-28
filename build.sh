af450735031d00b1137b75046cd669637d7e5267
Kim Jansson <kumpe82@gmail.com>
2022-03-28 10:12:31 +0100

    cleanup again

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-#go build  -tags netgo -a -v
-
 cd server
 go build
-
-# docker build --no-cache -t proxy-server:1 .
-# docker tag proxytest:1 localhost/proxy-server:1
-# docker push localhost/proxy-server:1
 
 cd ../sidecar
 docker build --no-cache -t proxy-sidecar:1 .
