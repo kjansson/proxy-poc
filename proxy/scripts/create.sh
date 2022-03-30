36285c51dbf9e9d5ea10b74ad6fe5b50a8cfe40c
Kim Jansson <kumpe82@gmail.com>
2022-03-30 20:38:05 +0100

    added port range

--- proxy/scripts/create.sh
+++ proxy/scripts/create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-iptables -t mangle -A OUTPUT -p udp --dport 161 -j MARK --set-mark 1
+iptables -t mangle -A OUTPUT -p udp --dport $PORT_RANGE -j MARK --set-mark 1
 
 ip route flush table 100
 ip rule add fwmark 1 lookup 100
